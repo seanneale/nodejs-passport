d4c6946d78b220bd1b36e6fc8a0a2e17ce9ce1a5
Jens H. Nielsen <jens@jens.hk>
2016-06-20 16:51:49 +0800

    Init commit

---
+++ config/passport.js
@@ -0,0 +1,76 @@
+
+// Load passport local
+var localStrategy = require('passport-local').Strategy;
+
+// Load user model
+var User = require('../model/user');
+
+module.exports = function( passport ) {
+
+  // Serialize user
+  passport.serializeUser( function( user, done){
+      done(null, user.id);
+  });
+
+  // Deserialize user
+  passport.deserializeUser(function(id, done){
+      User.findById(id, function(err, user){
+        done(err, user);
+      });
+  });
+
+  // Passport signup
+  passport.use('local-signup', new localStrategy({
+      usernameField : 'email',
+      passwordField : 'password',
+      passReqToCallback: true
+    },
+    function( req, email, password, done){
+        process.nextTick(function(){
+          User.findOne( {'local.email' : email }, function(err, user){
+            if(err){
+              return done(err);
+            }
+            if(user){
+              return done(null, false, req.flash('SignupMessage','That email is already in use'));
+            }else{
+              var newUser = new User();
+              newUser.local.email = email;
+              newUser.local.password = newUser.generateHash(password);
+              newUser.save(function(err){
+                if(err){
+                  console.log(err);
+                }
+                return done(null, newUser);
+              });
+            }
+          });
+        });
+    }));
+
+  // Passport login
+  passport.use('local-login', new localStrategy({
+      usernameField : 'email',
+      passwordField : 'password',
+      passReqToCallback: true
+    },
+    function( req, email, password, done){
+        process.nextTick(function(){
+          User.findOne( {'local.email' : email }, function(err, user){
+            if(err){
+              return done(err);
+            }
+
+            if(!user){
+              return done(null,false, req.flash('loginMassage', 'sorry no one by that email'));
+            }
+
+            if(!user.validPassword(password)){
+              return done(null,false, req.flash('loginMassage', 'sorry wrong password'));
+             }
+
+            return done(null, user);
+          });
+        });
+    }));
+}
